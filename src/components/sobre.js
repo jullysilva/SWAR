6721ec8ef8cc1103115d57be1a28a8a5d7463360
Jully Silva <jkasilva@sga.pucminas.br>
2019-12-06 00:22:23 -0300

    sobre.js - WCAG

---
+++ src/components/sobre.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export class sobre extends Component {
+    render() {
+        return (
+            <div>
+                1.1 - Colocar textos alternativos em conteúdo não-textual
+1.2 - Alternativas para áudios e vídeos
+1.3 - Organizar o código com boa estrutura lógica
+1.4 - Não use cores muito parecidas e não reproduza mídia automaticamente
+
+2.1 - Deixar todo o conteúdo acessível para usuários que usam somente o teclado (tab)
+2.2 - Deixe o usuário ter controle sobre conteúdos que tenham tempo de aparição
+2.3 - Nenhum conteúdo deve piscar mais de três vezes por segundo
+2.4 - Criar mecanismos de navegação úteis e claros ('Skip to Content', 'Click Here')
+
+3.1 - Colocar o idioma na página
+3.2 - Mostrar de forma clara aonde o usuário se encontra na navegação
+3.3 - Fornecer intruções claras ao usuário
+
+4.1 - Por fim, use todas as boas práticas de programação para criar um código acessível
+            </div>
+        )
+    }
+}
+
+export default sobre
+
